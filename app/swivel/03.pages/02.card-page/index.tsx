db2f4aede2840b5e0a949dc4031bddf1d4d5ade9
gran007 <gran007@naver.com>
2025-07-29 05:40:21 +0900

    # complete first page

---
+++ app/swivel/03.pages/02.card-page/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import style from './style.module.css';
+
+const CardPage = ({ show }: { show: boolean }) => {
+    return (
+        <section className={style.cardSection}>
+            
+        </section>
+    )
+}
+
+export default CardPage;
